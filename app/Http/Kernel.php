c4588a577dcbb17cdc593e3bc4021a9ed32b28ea
Nabeelshakeel401 <nabeel.shakeel401@gmail.com>
2025-09-27 01:11:09 +0500

    Latest changes with app bridge integration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'allow.shopify.iframe' => \App\Http\Middleware\AllowShopifyIFrame::class,
     ];
 }
